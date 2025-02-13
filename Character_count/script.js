fca7676d3fab11be19d1826e892151596b0663c4
Francisco Cruz <franciscohuertas25@gmail.com>
2025-02-13 15:55:03 -0500

    Added script.js

---
+++ Character_count/script.js
@@ -0,0 +1,9 @@
+// Get the elements
+let text = document.querySelector("#text");
+let charCount = document.querySelector("#character-count");
+
+// Listen for changes to the text area content
+text.addEventListener("input", function () {
+  // Display the characters count
+  charCount.textContent = text.value.length;
+});
