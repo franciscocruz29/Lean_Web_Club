09a1960a584932d9eb3cfa4b8a4006fc69bc67fb
Francisco Cruz <franciscohuertas25@gmail.com>
2025-03-04 10:08:56 -0500

    Update script.js

--- Character_count/script.js
+++ Character_count/script.js
@@ -3,7 +3,7 @@ let text = document.querySelector("#text");
 let charCount = document.querySelector("#character-count");
 
 // Listen for changes to the text area content
-text.addEventListener("input", function () {
+text.addEventListener("input", function() {
   // Display the characters count
   charCount.textContent = text.value.length;
 });
